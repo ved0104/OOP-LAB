de5486e5ee7b0708def12690f422678d11daf1c6
Vedang Dubey <dubeyved04@gmail.com>
2024-11-24 20:51:39 +0530

    updated folder name

--- Lab 4,5/1.cpp
+++ Lab 4,5/1.cpp
@@ -3,9 +3,9 @@
 // at least Rs. 2000/- of the earning to others. The input contains name and earning of an employee and the 
 // desired output is name and bonus to be paid to the employee.
 // Create a class to represent an employee. It should incluse the following:
-//  Data Members:
+//  Data Members:    
 //  Name
-//  Earning
+//  Earn
 //  Bonus
 //  Member Functions:
 //  To input data
